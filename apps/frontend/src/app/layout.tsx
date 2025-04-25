56e34c7a9088723d8fc03ef1e917b272b4770889
Cuddles47 <binbill472003@gmail.com>
2025-04-25 10:49:53 +0700

    Add suppressHydrationWarning to html tag in RootLayout

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${satoshi.variable} antialiased min-h-screen flex flex-col`}
       >
